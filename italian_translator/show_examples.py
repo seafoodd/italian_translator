f70be2000ec5dc7f0f6ac012ad582b4c04ec77f0
seafood <mail.seafood@proton.me>
2024-10-01 12:35:20 +0300

    improved text cleaning in show_examples.py

--- italian_translator/show_examples.py
+++ italian_translator/show_examples.py
@@ -1,6 +1,13 @@
 import re
 from colorama import Fore, Style
 
+def clean_text(text):
+    text = re.sub(r'<.*?>', '#', text)
+    for i in range(2, 5):
+        text = text.replace('#' * i, '#')
+    text = re.sub(r'#(.*?)#', lambda m: f"{Fore.BLUE}{m.group(1)}{Style.RESET_ALL}", text)
+    return '\n'.join(text.strip().split('\n'))
+
 def show_examples(soup, num_examples):
     examples_content = soup.find('section', id='examples-content')
 
@@ -10,29 +17,17 @@ def show_examples(soup, num_examples):
 
     examples = examples_content.find_all('div', class_='example')
     print(f"{Fore.GREEN}Examples:")
+
     for example in examples[:num_examples]:
         src_div = example.find('div', class_='src')
         trg_div = example.find('div', class_='trg')
         text_src = src_div.find('span', class_='text').decode_contents()
         text_trg = trg_div.find('span', class_='text').decode_contents()
 
-        cleaned_text_src = re.sub(r'<.*?>', '#', text_src)
-        cleaned_text_trg = re.sub(r'<.*?>', '#', text_trg)
+        cleaned_text_src = clean_text(text_src)
+        cleaned_text_trg = clean_text(text_trg)
 
-        cleaned_text_src = cleaned_text_src.replace('##', '#')
-        cleaned_text_trg = cleaned_text_trg.replace('##', '#')
-        cleaned_text_src = cleaned_text_src.replace('###', '#')
-        cleaned_text_trg = cleaned_text_trg.replace('###', '#')
-        cleaned_text_src = cleaned_text_src.replace('####', '#')
-        cleaned_text_trg = cleaned_text_trg.replace('####', '#')
-
-        cleaned_text_src = re.sub(r'#(.*?)#', lambda m: f"{Fore.BLUE}{m.group(1)}{Style.RESET_ALL}", cleaned_text_src)
-        cleaned_text_trg = re.sub(r'#(.*?)#', lambda m: f"{Fore.BLUE}{m.group(1)}{Style.RESET_ALL}", cleaned_text_trg)
-
-        separated_text_src = '\n'.join(cleaned_text_src.strip().split('\n'))
-        separated_text_trg = '\n'.join(cleaned_text_trg.strip().split('\n'))
-
-        print(f' - {separated_text_src}')
-        print(f' - {separated_text_trg}')
+        print(f' - {cleaned_text_src}')
+        print(f' - {cleaned_text_trg}')
         print()
 
